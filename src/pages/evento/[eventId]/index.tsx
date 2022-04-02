eddcabe41bfa4a96e3d31127f4691c036aa2a0f9
CRamos132 <camille.gpramos@gmail.com>
2022-04-02 18:22:37 -0300

    updated event page

--- src/pages/evento/[eventId]/index.tsx
+++ src/pages/evento/[eventId]/index.tsx
@@ -1,12 +1,23 @@
+import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
+import { useQuery } from "react-query";
 import PageWrapper from "../../../components/PageWrapper";
+import { firestore } from "../../../lib/firebase";
+import { Event } from "../../../types";
 
 export default function EventPage() {
   const router = useRouter()
   const { eventId } = router.query
+  const { isLoading, data } = useQuery(`event-${eventId}`, async () => {
+    const docRef = doc(firestore, "eventos", eventId as string);
+    const docSnap = await getDoc(docRef);
+    console.log("🚀 ~ docSnap", docSnap.data())
+    return docSnap.data() as Event
+  })
   return (
     <PageWrapper>
-      <h1>Evento - {eventId}</h1>
+      {isLoading && (<p>Carregando...</p>)}
+      <h1>Evento - {data?.titulo}</h1>
     </PageWrapper>
   )
 }
