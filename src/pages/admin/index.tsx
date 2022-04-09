695baabb6ec3b9eaeccbd38f0ce7820a221a7942
CRamos132 <camille.gpramos@gmail.com>
2022-04-09 16:16:19 -0300

    updated admin page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,11 +1,15 @@
+import { Button } from "@chakra-ui/react";
 import Link from "next/link";
 import PageWrapper from "../../components/PageWrapper";
 
 export default function AdminPage() {
   return (
     <PageWrapper>
-      Estamos trabalhando nisso...
-      <Link href='/admin/relatorio'>Relatórios</Link>
+      <Link href='/admin/relatorio' passHref>
+        <Button>
+          Relatório de usuários
+        </Button>
+      </Link>
     </PageWrapper>
   )
 }
