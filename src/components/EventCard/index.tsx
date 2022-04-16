735347fae7afdc7d6438461d15c07be032135985
CRamos132 <camille.gpramos@gmail.com>
2022-04-16 15:19:01 -0300

    updated event card

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -17,11 +17,30 @@ interface IProps {
 
 export default function EventCard({ event }: IProps) {
   const auth = useAuth()
+  const getDate = () => {
+    const date = new Date(event?.tempo)
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+  }
   return (
-    <Flex as='button' direction='column' alignItems='flex-end' cursor='pointer'>
+    <Flex as='button' direction='column' alignItems='flex-end' cursor='pointer' >
       <Link href={`/evento/${event.id}`} passHref>
-        <Flex minHeight='150px' width='250px' display='flex' justifyContent='center' borderRadius='24px' backgroundColor='blue.200'>
-          <Box>{event?.apelido || event.titulo}</Box>
+        <Flex
+          minHeight='150px'
+          width='250px'
+          display='flex'
+          flexDirection='column'
+          justifyContent='flex-start'
+          borderRadius='24px'
+          backgroundColor='blue.200'
+          padding='8px'
+        >
+          <Box width='100%' as='h2' fontSize='1.2rem' fontWeight='bold'>{event?.apelido || event.titulo}</Box>
+          <Box>{getDate()} - {event?.cidade}, {event?.uf}</Box>
+          <Box>{Object.keys(event?.jogos).map((item) => {
+            if ((event?.jogos as any)?.[item]) {
+              return <Box key={item} textTransform='capitalize'>{item}</Box>
+            }
+          })}</Box>
         </Flex>
       </Link>
       {
