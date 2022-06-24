d6d6de5e527b38e228eaaa128861504888652361
Thiago de Jesus <99thiagodejesus@gmail.com>
2022-06-24 19:36:10 -0300

    feat: add users page

--- src/lib/firebase/UsersRepository.ts
+++ src/lib/firebase/UsersRepository.ts
@@ -1,6 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { firestore } from "../firebase";
-import { collection, query, getDocs, orderBy as firestoreOrderBy } from "firebase/firestore";
-import { IUser } from "../../interfaces/User";
+import {
+  collection,
+  query,
+  getDocs,
+  orderBy as firestoreOrderBy,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { IFirebaseUserData, IUser } from "../../interfaces/User";
 
 interface IGetUsersArgs {
   orderBy?: string;
@@ -12,10 +21,7 @@ export async function getUsers({ orderBy }: IGetUsersArgs): Promise<IUser[]> {
     queryArguments.push(firestoreOrderBy(orderBy));
   }
 
-  const usersQuery = query(
-    collection(firestore, "users"),
-    ...queryArguments
-  );
+  const usersQuery = query(collection(firestore, "users"), ...queryArguments);
 
   const usersQuerySnapshot = await getDocs(usersQuery);
   const users: IUser[] = [];
@@ -26,4 +32,28 @@ export async function getUsers({ orderBy }: IGetUsersArgs): Promise<IUser[]> {
   });
 
   return users;
+}
+
+export async function getUserById(userId: string): Promise<IFirebaseUserData> {
+  const docRef = doc(firestore, "users", userId as string);
+  const docSnap = await getDoc(docRef);
+  return { id: docSnap.id, ...docSnap.data() } as IFirebaseUserData;
+}
+
+interface IUpdateUserArgs {
+  userId: string;
+  userData: IFirebaseUserData;
+}
+
+export async function updateUser({ userId, userData }: IUpdateUserArgs) {
+  const response = updateDoc(doc(firestore, "users", userId as string), {
+    ...userData,
+  })
+    .then((data) => {
+      return {success: true, error: false};
+    })
+    .catch((error) => {
+      return { success: false, error };
+    });
+  return response;
 }
