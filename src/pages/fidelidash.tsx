d6bf1d9052bbfa2643ffdff8fdf1940a0aea50bb
Thiago de Jesus <99thiagodejesus@gmail.com>
2022-08-04 21:53:02 -0300

    fix: fix fidelidash order and exhibition name

--- src/pages/fidelidash.tsx
+++ src/pages/fidelidash.tsx
@@ -8,12 +8,31 @@ import { rankBorders } from "../utils/rank";
 import { CustomLink } from "../components/CustomLink";
 import Head from "next/head";
 
+const fidelidashOrder = {
+  platina: 0,
+  ouro: 1,
+  prata: 2,
+  bronze: 3,
+  last: 1000,
+};
+
+const sortFidelidash = (fidelidash: IUser[]) => {
+  return fidelidash.sort(
+    (a, b) =>
+      fidelidashOrder[a?.firebaseData?.fidelidash || "last"] -
+      fidelidashOrder[b.firebaseData?.fidelidash || "last"]
+  );
+};
+
 export default function FidelidashPage() {
   const [fidelidash, setFidelidash] = useState<IUser[]>([] as IUser[]);
 
   async function getFidelidash() {
     const fidelidashUsers = await getUsers({ orderBy: "fidelidash" });
-    setFidelidash(fidelidashUsers);
+    const fidelidashUsersFiltered = fidelidashUsers.filter(
+      (u) => u.firebaseData?.fidelidash
+    );
+    setFidelidash(sortFidelidash(fidelidashUsersFiltered));
     return fidelidashUsers;
   }
 
@@ -24,58 +43,57 @@ export default function FidelidashPage() {
       <Head>
         <title>Ligas</title>
       </Head>
-      <Flex direction="column" alignItems="center">
-        <Heading mt="4" fontSize={30}>
-          Fidelidash
-        </Heading>
+      <Heading mt="4" fontSize={30} textAlign="center">
+        Fidelidash
+      </Heading>
 
-        <Flex direction="column" gap="4" mt="4">
-          {fidelidash.map((user) => {
-            return (
-              <CustomLink
-                key={user.id}
-                width="100%"
-                href={`/user/${user.id}`}
-                color="initial"
+      <Flex direction="column" gap="4" mt="4" alignItems="center">
+        {fidelidash.map((user) => {
+          return (
+            <CustomLink
+              key={user.id}
+              width="100%"
+              href={`/user/${user.id}`}
+              color="initial"
+              maxWidth="300px"
+            >
+              <Grid
+                minWidth="fit-content"
+                templateColumns=".2fr .8fr"
+                alignItems="center"
+                justifyContent="center"
               >
-                <Grid
-                  width="100%"
-                  templateColumns=".25fr .75fr"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  {user.firebaseData?.foto !== "img/default-profile.png" ? (
-                    <Image
-                      height={12}
-                      width={12}
-                      borderRadius="50%"
-                      src={user.firebaseData?.foto}
-                      alt={user.firebaseData?.nome}
-                      border={
-                        user.firebaseData?.fidelidash
-                          ? rankBorders[user.firebaseData?.fidelidash]
-                          : ""
-                      }
-                    />
-                  ) : (
-                    <Box
-                      height={12}
-                      width={12}
-                      borderRadius="50%"
-                      backgroundColor="gray.400"
-                      border={
-                        user.firebaseData?.fidelidash
-                          ? rankBorders[user.firebaseData?.fidelidash]
-                          : ""
-                      }
-                    />
-                  )}
-                  <Text ml={4}>{user.firebaseData?.nome}</Text>
-                </Grid>
-              </CustomLink>
-            );
-          })}
-        </Flex>
+                {user.firebaseData?.foto !== "img/default-profile.png" ? (
+                  <Image
+                    height={12}
+                    width={12}
+                    borderRadius="50%"
+                    src={user.firebaseData?.foto}
+                    alt={user.firebaseData?.apelido}
+                    border={
+                      user.firebaseData?.fidelidash
+                        ? rankBorders[user.firebaseData?.fidelidash]
+                        : ""
+                    }
+                  />
+                ) : (
+                  <Box
+                    height={12}
+                    width={12}
+                    borderRadius="50%"
+                    backgroundColor="gray.400"
+                    border={
+                      user.firebaseData?.fidelidash
+                        ? rankBorders[user.firebaseData?.fidelidash]
+                        : ""
+                    }
+                  />
+                )}
+                <Text ml={4}>{user.firebaseData?.apelido}</Text>
+              </Grid>
+            </CustomLink>
+          );
+        })}
       </Flex>
     </PageWrapper>
   );
